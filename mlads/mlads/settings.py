40e62cf42a07452a107963c1715bee016ebb9f4a
praise_olawoore <olawoorejohn@gmail.com>
2022-09-08 07:05:36 +0100

    ads sys

--- mlads/mlads/settings.py
+++ mlads/mlads/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-m2*i6(#a$x+ai^aokf8yp96n^otx7fa9r)lq+@_t_s$hb-vaev"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
